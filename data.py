2de679231fb43458dc34ec57bbdf8752562da45c
Uladzislau Chabukin <vladchebukin@yandex.ru>
2024-10-03 21:00:18 +0300

    Рефакторинг тестов с фиксацией параметров

--- data.py
+++ data.py
@@ -15,3 +15,35 @@ ingredients = [
     [filling_type, "dinosaur", 200],
     [filling_type, "sausage", 300]
 ]
+
+bun_names = [
+    "black bun",
+    "white bun",
+    "red bun",
+]
+
+bun_prices = [
+    100,
+    200,
+    300
+]
+
+ingredient_types = [
+    'SAUCE',
+    'FILLING'
+]
+
+ingredient_names = [
+    "hot sauce",
+    "sour cream",
+    "chili sauce",
+    "cutlet"
+    "dinosaur"
+    "sausage"
+]
+
+ingredient_prices = [
+    100,
+    200,
+    300
+]
